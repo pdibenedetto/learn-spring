a6305855954706dab5f02156b0123f5c876374e1
DiBenedetto, Paul <pdibenedetto@users.noreply.github.com>
2019-06-25 12:41:28 -0400

    Updating author

--- m3-logging-in-a-spring-boot-project-start/src/main/java/com/baeldung/ls/service/impl/ProjectServiceImpl.java
+++ m3-logging-in-a-spring-boot-project-start/src/main/java/com/baeldung/ls/service/impl/ProjectServiceImpl.java
@@ -1,29 +1,37 @@
 package com.baeldung.ls.service.impl;
 
-import java.util.Optional;
-
-import org.springframework.stereotype.Service;
-
 import com.baeldung.ls.persistence.model.Project;
 import com.baeldung.ls.persistence.repository.IProjectRepository;
 import com.baeldung.ls.service.IProjectService;
+import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
 @Service
-public class ProjectServiceImpl implements IProjectService {
+public class ProjectServiceImpl implements IProjectService
+{
+
+    private static Logger LOG = LoggerFactory.getLogger(ProjectServiceImpl.class);
 
     private IProjectRepository projectRepository;
 
-    public ProjectServiceImpl(IProjectRepository projectRepository) {
+    public ProjectServiceImpl(IProjectRepository projectRepository)
+    {
         this.projectRepository = projectRepository;
     }
 
     @Override
-    public Optional<Project> findById(Long id) {
+    public Optional<Project> findById(Long id)
+    {
+        LOG.debug("Project Service >> Finding Project By Id {}", id);
         return projectRepository.findById(id);
     }
 
     @Override
-    public Project save(Project project) {
+    public Project save(Project project)
+    {
+        LOG.debug("Project Service >> Saving Project {}", project);
         return projectRepository.save(project);
     }
 
